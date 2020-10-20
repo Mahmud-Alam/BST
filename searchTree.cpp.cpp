7daea9325062cb796f9038d0270b80670d0731f3
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-20 18:15:59 +0600

    -

--- searchTree.cpp.cpp
+++ searchTree.cpp.cpp
@@ -16,3 +16,22 @@ class Node{
     }
     ~Node();
 };
+
+Node* createTree(int key){
+    int value;
+    Node* node = new Node(key);
+    if(key == 1024)
+        return NULL;
+    cout<<key<<"'s Left key: ";cin>>value;
+    node->left = createTree(value);
+    cout<<key<<"'s RIght key: ";cin>>value;
+    node->right = createTree(value);
+}
+
+void postOrder(Node* root){
+    if(root==NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
